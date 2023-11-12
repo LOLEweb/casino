3fec769eba8dcd9f1ee3ecd9bfb93fd1f22a73e9
LOLEweb <priveta22@mail.ru>
2023-11-12 21:43:16 +0300

    add folders

---
+++ app/components/Shop.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Shop = () => {
+  return (
+    <>
+        <main className='main bg-[#0d0d0d] h-screen'>
+            1
+        </main>
+    </>
+  )
+}
+
+export default Shop
