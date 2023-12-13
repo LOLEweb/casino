4e7166aee514869c4f7736b273f3740983e0f2b4
LOLEweb <priveta22@mail.ru>
2023-12-13 22:07:59 +0300

    fix bags

--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -1,13 +1,20 @@
+'use client'
 import React from 'react'
 import Image from 'next/image'
 import avatar from './../assets/avatar.png'
 import iconDise from './../assets/icon-dise.svg'
-import iconSHop from './../assets/akar-icons_cart.svg'
 import iconLeave from './../assets/icon-off.svg'
+import iconShop from './../assets/akar-icons_cart.svg'
 import Link from 'next/link'
 
+// backend files
+import file_back from './../backend/app.js'
+import {signIn, signOut} from 'next-auth/react'
+import {useSession} from "next-auth/react"
+//
 
 const Sidebar = () => {
+  const {status} = useSession()
   return (
     <>
     <div className=' bg-[#131313] sidebar pt-4 lg:pl-6 min-h-full fixed lg:block flex flex-col items-center'>
@@ -27,15 +34,24 @@ const Sidebar = () => {
           <li className=' py-3'>
             <Link href="/shop" legacyBehavior>
             <a className='flex gap-4 duration-300 hover:text-[#794DFD]'>
-              <Image className='lg:w-5 lg:h-5 w-6 h-6' src={iconSHop} alt={'icon'} />
+              <Image className='lg:w-5 lg:h-5 w-6 h-6' src={iconShop} alt={'icon'} />
               <p className='lg:block hidden'>Магазин</p>
             </a>
           </Link>
           </li>
           <li className=' py-3'>
-            <a href="#" className='flex gap-4 duration-300 hover:text-[#794DFD]'>
-            <Image className='lg:w-5 lg:h-5 w-6 h-6' src={iconLeave} alt={'icon'} />
-              <p className='lg:block hidden'>Log out</p>
+            <a href="#"  className='flex gap-4 duration-300 hover:text-[#794DFD]' >
+              {status === "authenticated" ? ( //Если игрок авторизован, то выводится Log Out, если нет, то Log in
+                <button onClick={() => signOut()}>
+                  <Image src={iconLeave} alt={'icon'} />
+                  <p  className='lg:block hidden'>Log out</p>
+                </button> /*При нажатие на кнопку производится авторизация*/
+              ): (
+                <button onClick={() => signIn('discord')}>
+                  <Image src={iconLeave} alt={'icon'} />
+                  <p  className='lg:block hidden'>Log in</p>
+                </button> /*При нажатие на кнопку вызывается функция */
+              )}
             </a>
           </li>
         </ul>
