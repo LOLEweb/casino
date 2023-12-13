9d1d6c6dd2b20dc61b943d1fbb245242cd4447be
xsel <65423939+Tadjikistan@users.noreply.github.com>
2023-12-13 21:45:51 +0300

    +db

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,3 +1,5 @@
+import { connectMongoDB } from "@/lib/mongodb";
+import User from "@/models/user.js"
 import NextAuth from "next-auth/next";
 import DiscordProvider from 'next-auth/providers/discord'
 
@@ -8,6 +10,35 @@ const authOptions = {
             clientSecret: process.env.DISCORD_CLIENT_SECRET,
         }),
     ],
+    callbacks: { //Получаем информацию о пользователе
+        async signIn({user, account}) {
+            console.log("User", user.id)
+            console.log('Acc', account.provider)
+            if (account.provider === 'discord') {
+                const {id, name, email} = user;
+                const balance = 300
+                try{
+                    await connectMongoDB();
+
+                    const userExists = await User.findOne({name})
+                    if (!userExists){
+                        const res = await fetch('http://localhost:3000/api/user', {
+                        method: "POST",
+                        headers: {"Content-type": "application/json",},
+                        body: JSON.stringify({id, name, email, balance})
+                    })
+                    if (res.ok) {
+                        return user;
+                    }
+                    }
+                    
+                } catch (error){
+                    console.log(error)
+                }
+            }
+            return user;
+        }
+    }
 };
 const handler = NextAuth(authOptions);
 
